0b083d6508508495a6aba24f46fd9a62e99a5ac0
김인태 <dlsxody1@naver.com>
2022-10-28 14:23:30 +0900

    problem1 solved

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,47 @@
 function problem1(pobi, crong) {
-  var answer;
+  let answer = 0;
+
+  let pobiPagePlus = pobi.map((el) => {
+    return el
+      .toString()
+      .split("")
+      .map((x) => parseInt(x))
+      .reduce((acc, arr) => acc + arr + 0);
+  });
+  let pobiPageMultiply = pobi.map((el) => {
+    return el
+      .toString()
+      .split("")
+      .map((x) => parseInt(x))
+      .reduce((acc, arr) => acc * arr);
+  });
+  let crongPagePlus = crong.map((el) => {
+    return el
+      .toString()
+      .split("")
+      .map((x) => parseInt(x))
+      .reduce((acc, arr) => acc + arr + 0);
+  });
+  let crongPageMultiply = crong.map((el) => {
+    return el
+      .toString()
+      .split("")
+      .map((x) => parseInt(x))
+      .reduce((acc, arr) => acc * arr);
+  });
+
+  const pobiArray = pobiPagePlus.concat(pobiPageMultiply);
+  const pobiMax = Math.max.apply(null, pobiArray);
+  const crongArray = crongPagePlus.concat(crongPageMultiply);
+  const crongMax = Math.max.apply(null, crongArray);
+  const pobiDifference = pobi[1] - pobi[0];
+  const crongDifference = crong[1] - crong[0];
+
+  if (pobiMax > crongMax) answer = 1;
+  else if (pobiMax < crongMax) answer = 2;
+  else answer = 0;
+
+  if (pobiDifference !== 1 || crongDifference !== 1) return (answer = -1);
   return answer;
 }
 
